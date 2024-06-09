7e539022480cf2f6b003dffffa48f5cb7e83cc75
Yeicot JJ Sarmiento Polo <yeicotsarmientopolo2@gmail.com>
2024-06-08 22:17:31 -0500

    subida de docs
    uwu pyerna

--- index.php
+++ index.php
@@ -12,32 +12,32 @@
     <div class="page-container">
         <section class="left-section">
             <nav class="sidebar-menu">
+                <div class="titleSideBar">
+                    <h2>PIURA HACK</h2>
+                    <h3>Hackaton Reto 2 : On Boarding</h3>
+                    <h4>Grupo 18</h4>
+                </div>
+                
                 <ul>
-                    <li><a href="#" onclick="showContent('content1')">General</a></li>
+                    <li><a href="#" onclick="showContent('content1')">Minijuegos</a></li>
                     <li><a href="#" onclick="showContent('content2')">Login</a></li>
-                    <li><a href="#" onclick="showContent('content3')">Tienda</a></li>
-                    <li><a href="#" onclick="showContent('content4')">Ventanas emergentes</a></li>
+                    <li><a href="#" onclick="showContent('content3')">Menú Principal</a></li>
+                    <li><a href="#" onclick="showContent('content4')">Tienda</a></li>
+                    <li><a href="#" onclick="showContent('content5')">Ventanas emergentes</a></li>
                 </ul>
             </nav>
         </section>
         <!--Seccion de minijuegos-->
         <section class="right-section">
-            <h2>Documentacion</h2>
+            <h2>Documentación</h2>
             <div id="content1" class="content">
-                <h1>Titulo: UTP + play</h1>
-                <img src="./img/inicial.png" width="600px">
-                <p>Una plataforma creada con la finalidad de mejorar el proceso de inducción de los nuevos ingresantes UTP, +play busca que el alumno aprenda a través de minijuegos y quizz interactivos.
-                </p>
-                <h2>Tecnologías</h2><!--Definir sus variables de los minijuegos-->
-                <p>El desarrollo de la plataforma se ha dividido en 3 sectores, a continuación se presentan las herramientas usadas por cada sector respectivamente:
-                <ul>
-                    <li><b>Diseño UI/UX:</b>Figma</li>
-                    <LI><b>Desarrollo frontend:</b>Html5, JS, CSS3 y boostrap5</LI>
-                    <li><b>Desarrollo Backend:</b>php y mysqlPHP my admin</li>
-                </ul>
-                </p>
-                <h2>Recursos:</h2>
-                <p>Enlace figma: <a href="https://www.figma.com/design/oQvntsbTtE1pSKL2dQi8Bf/HACKATON-RETO-2-ONBOARDING-FIGMA?node-id=251-18&t=6m47Lq66OwummRKg-1" target="_blank">Presionar aquí</a></p>
+                <h1>Titulo: Minijuegos</h1>
+                <img src="https://via.placeholder.com/300">
+                <p>Ingrese el titulo</p>
+                <h2>Variables</h2><!--Definir sus variables de los minijuegos-->
+                <p>Ingrese variables</p>
+                <h2>Eventos</h2>
+                <p>Ingrese eventos</p>
             </div>
             <!--Seccion de Login-->
             <div id="content2" class="content" style="display: none;">
@@ -49,8 +49,36 @@
                 <h2>Eventos</h2>
                 <p>Ingrese eventos</p>
             </div>
-            <!--Seccion de tienda-->
+            <!--Seccion de Menú-->
             <div id="content3" class="content" style="display: none;">
+                <h1>Titulo: Menú Principal</h1>
+                <div class="recursosMenu">
+                    <img src="./recursos/menu1.PNG" class="menu1 imgmenu">
+                    <img src="./recursos/menu2.PNG" class="menu2 imgmenu">
+                </div>
+                <h2>Variables</h2><!--Definir sus variables uwu-->
+                <p>
+                <ul>
+                    <li>level(int): nivel actual del jugador</li>
+                    <li>coins(int): monedas actuales del jugador</li>
+                    <li>name(String): nombre del alumno</li>
+                    <li>levelComplete(Boolean): si el nivel está completo</li>
+                    <li>curveComplete(Boolean): si la linea del mapa está completa</li>
+                </ul>
+                </p>
+                <h2>Eventos</h2>
+                <p>
+                <ul>
+                    <li>Recompensar con UtpCoins</li>
+                    <li>Mostrar el total de UtpCoins</li>
+                    <li>Guíar al alumno a través del flujo de niveles</li>
+                    <li>Mostrar el progreso del alumno en el flujo de niveles</li>
+                    <li>Interactuar con el alumno por medio del selector de niveles</li>
+                </ul>
+                </p>
+            </div>
+            <!--Seccion de tienda-->
+            <div id="content4" class="content" style="display: none;">
                 <h1>Titulo: Tienda</h1>
                 <img src="https://via.placeholder.com/300">
                 <p>Ingrese el titulo</p>
@@ -60,7 +88,7 @@
                 <p>Ingrese eventos</p>
             </div>
             <!--Seccion ventanas emergentes-->
-            <div id="content4" class="content" style="display: none;">
+            <div id="content5" class="content" style="display: none;">
                 <h1>Titulo: Ventanas emergentes</h1>
                 <img src="https://via.placeholder.com/300">
                 <p>Ingrese el titulo</p>
@@ -71,7 +99,123 @@
             </div>
         </section>
     </div>
+    <div id="particles-js"></div>
     <script src="scripts.js"></script>
+    <script src="particles.min.js"></script>
+    <script>
+        particlesJS(
+            {
+                "particles": {
+                    "number": {
+                        "value": 80,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
+                        }
+                    },
+                    "color": {
+                        "value": "#0c2b57"
+                    },
+                    "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                            "color": "#000000"
+                        },
+                        "polygon": {
+                            "nb_sides": 5
+                        },
+                        "image": {
+                            "src": "img/github.svg",
+                            "width": 100,
+                            "height": 100
+                        }
+                    },
+                    "opacity": {
+                        "value": 0.5,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 1,
+                            "opacity_min": 0.1,
+                            "sync": false
+                        }
+                    },
+                    "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
+                    },
+                    "line_linked": {
+                        "enable": true,
+                        "distance": 150,
+                        "color": "#b12828",
+                        "opacity": 0.4,
+                        "width": 1
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                    }
+                },
+                "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "push"
+                        },
+                        "resize": true
+                    },
+                    "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 8,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 200,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                },
+                "retina_detect": true
+            }
+        )
+    </script>
 </body>
 
 </html>
